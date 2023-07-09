5b0a8a8445bb06de61ebace509672870eb140949
DeveshGautam2000 <deveshgautamking@gmail.com>
2023-07-09 19:44:24 +0530

    update file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 # Clear existing scene
 bpy.ops.object.select_all(action='DESELECT')
@@ -25,98 +26,50 @@ faces = [
     (0, 1, 3, 2)
 ]
 
-# Create plain glasses
-plain_glasses_mesh = bpy.data.meshes.new('PlainGlassesMesh')
-plain_glasses_mesh.from_pydata(vertices, [], faces)
+glasses_mesh = bpy.data.meshes.new('GlassesMesh')
+glasses_mesh.from_pydata(vertices, [], faces)
 
-plain_glasses_obj = bpy.data.objects.new(
-    'PlainGlassesObject', plain_glasses_mesh)
-scene.collection.objects.link(plain_glasses_obj)
+glasses_obj = bpy.data.objects.new('GlassesObject', glasses_mesh)
+scene.collection.objects.link(glasses_obj)
 
-# Create plain glass material
-plain_glass_material = bpy.data.materials.new('PlainGlassMaterial')
-plain_glasses_obj.data.materials.append(plain_glass_material)
-
-# Set up plain glass material properties
-plain_glass_material.use_nodes = True
-plain_glass_material.node_tree.nodes.clear()
-plain_glass_material_output = plain_glass_material.node_tree.nodes.new(
+# Set up glass material
+glass_material = bpy.data.materials.new('GlassMaterial')
+glass_material.use_nodes = True
+glass_material.node_tree.nodes.clear()
+glass_material_output = glass_material.node_tree.nodes.new(
     'ShaderNodeOutputMaterial')
-plain_glass_bsdf = plain_glass_material.node_tree.nodes.new(
-    'ShaderNodeBsdfGlass')
-plain_glass_material.node_tree.links.new(
-    plain_glass_bsdf.outputs['BSDF'], plain_glass_material_output.inputs['Surface'])
+glass_material_bsdf = glass_material.node_tree.nodes.new(
+    'ShaderNodeBsdfPrincipled')
+glass_material.node_tree.links.new(
+    glass_material_bsdf.outputs['BSDF'], glass_material_output.inputs['Surface'])
+glass_material_bsdf.inputs['Base Color'].default_value = (
+    0.2, 0.4, 0.8, 1.0)  # Adjust the RGB values as desired
+glass_material_bsdf.inputs['Transmission'].default_value = 1.0
+glass_material_bsdf.inputs['Transmission Roughness'].default_value = 0.0
+glasses_obj.data.materials.append(glass_material)
 
-# Set the render output path for plain glasses
-plain_glasses_render_output_path = '/path/to/save/plain_glasses_render.png'
+# Set up the render settings
 scene.render.image_settings.file_format = 'PNG'
-scene.render.filepath = plain_glasses_render_output_path
 
-# Render the plain glasses scene
-bpy.ops.render.render(write_still=True)
+# Set the panoramic camera properties
+camera.data.type = 'PANO'
+camera.data.cycles.panorama_type = 'EQUIRECTANGULAR'
 
-# Create textured glasses
-textured_glasses_mesh = bpy.data.meshes.new('TexturedGlassesMesh')
-textured_glasses_mesh.from_pydata(vertices, [], faces)
+# Define the number of steps to capture the panoramic view
+num_steps = 36  # Adjust this value to control the level of detail
 
-textured_glasses_obj = bpy.data.objects.new(
-    'TexturedGlassesObject', textured_glasses_mesh)
-scene.collection.objects.link(textured_glasses_obj)
+# Set the render output path and base filename
+output_path = '/path/to/save/panoramic_view_'
+output_file_extension = '.png'
 
-# Create textured glass material
-textured_glass_material = bpy.data.materials.new('TexturedGlassMaterial')
-textured_glasses_obj.data.materials.append(textured_glass_material)
+# Capture the panoramic view by rotating the camera
+for step in range(num_steps):
+    angle = 2 * math.pi * (step / num_steps)
+    camera.rotation_euler[2] = angle
 
-# Set up textured glass material properties
-textured_glass_material.use_nodes = True
-textured_glass_material.node_tree.nodes.clear()
-textured_glass_material_output = textured_glass_material.node_tree.nodes.new(
-    'ShaderNodeOutputMaterial')
-textured_glass_bsdf = textured_glass_material.node_tree.nodes.new(
-    'ShaderNodeBsdfGlass')
-textured_glass_texture = textured_glass_material.node_tree.nodes.new(
-    'ShaderNodeTexNoise')
-textured_glass_texture.inputs['Scale'].default_value = 5.0
-textured_glass_material.node_tree.links.new(
-    textured_glass_texture.outputs['Color'], textured_glass_bsdf.inputs['Color'])
-textured_glass_material.node_tree.links.new(
-    textured_glass_bsdf.outputs['BSDF'], textured_glass_material_output.inputs['Surface'])
+    # Set the output filename for the current step
+    scene.render.filepath = output_path + \
+        str(step).zfill(3) + output_file_extension
 
-# Set the render output path for textured glasses
-textured_glasses_render_output_path = '/path/to/save/textured_glasses_render.png'
-scene.render.filepath = textured_glasses_render_output_path
-
-# Render the textured glasses scene
-bpy.ops.render.render(write_still=True)
-
-# Create glossy glasses
-glossy_glasses_mesh = bpy.data.meshes.new('GlossyGlassesMesh')
-glossy_glasses_mesh.from_pydata(vertices, [], faces)
-
-glossy_glasses_obj = bpy.data.objects.new(
-    'GlossyGlassesObject', glossy_glasses_mesh)
-scene.collection.objects.link(glossy_glasses_obj)
-
-# Create glossy glass material
-glossy_glass_material = bpy.data.materials.new('GlossyGlassMaterial')
-glossy_glasses_obj.data.materials.append(glossy_glass_material)
-
-# Set up glossy glass material properties
-glossy_glass_material.use_nodes = True
-glossy_glass_material.node_tree.nodes.clear()
-glossy_glass_material_output = glossy_glass_material.node_tree.nodes.new(
-    'ShaderNodeOutputMaterial')
-glossy_glass_bsdf = glossy_glass_material.node_tree.nodes.new(
-    'ShaderNodeBsdfGlass')
-glossy_glass_material.node_tree.links.new(
-    glossy_glass_bsdf.outputs['BSDF'], glossy_glass_material_output.inputs['Surface'])
-glossy_glass_material.node_tree.nodes.new('ShaderNodeBsdfGlossy')
-glossy_glass_material.node_tree.links.new(
-    glossy_glass_bsdf.outputs['BSDF'], glossy_glass_material_output.inputs['Surface'])
-
-# Set the render output path for glossy glasses
-glossy_glasses_render_output_path = '/path/to/save/glossy_glasses_render.png'
-scene.render.filepath = glossy_glasses_render_output_path
-
-# Render the glossy glasses scene
-bpy.ops.render.render(write_still=True)
+    # Render the current step
+    bpy.ops.render.render(write_still=True)
